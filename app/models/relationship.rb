f31059da16cca9728ce9028bea4557bf27f8f104
ahuiru <zhuangerlangyou@gmail.com>
2022-03-02 11:26:22 +0900

    follow、chat

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -4,4 +4,8 @@ class Relationship < ApplicationRecord
 
   validates :user_id, presence: true
   validates :follow_id, presence: true, uniqueness: {scope: :user_id}
+
+  def following?(other_user)
+    self.followings.include?(other_user)
+  end
 end
