8800cf544fa98e83782789c77cd3a40c0899d6f7
ahuiru <zhuangerlangyou@gmail.com>
2022-03-19 02:12:15 +0900

    homeでのlike-button

--- app/controllers/ranks_controller.rb
+++ app/controllers/ranks_controller.rb
@@ -1,6 +1,6 @@
 class RanksController < ApplicationController
 
   def index
-    @photo = Photo.find(Like.group(:photo_id).order('count(photo_id) desc').limit(10).pluck(:photo_id))
+    @photo = Photo.find(Like.group(:photo_id).order('count(photo_id) desc').limit(5).pluck(:photo_id))
   end
 end
